8361c934493ffe63d2300046c527955492bdcd68
Desire Martins <desiremartins20@gmail.com>
2026-03-30 08:43:54 -0300

    adicionei densidade, pibpercapta e poder super

--- projeto_supertrunfo.c
+++ projeto_supertrunfo.c
@@ -10,6 +10,8 @@ int main(){
     float areaemkm²1;
     float PIB1;
     float densidade1;
+    float pibpercapta1;
+    float super1;
 
     //Definição das variáveis para carta 2
     char Estado2;
@@ -19,6 +21,9 @@ int main(){
     int numerodepontosturisticos2;
     float areaemkm²2;
     float PIB2;
+    float densidade2;
+    float pibpercapta2;
+    float super2;
 
     //Cadastro da carta 1
     printf("Digite o nome do Estado: \n");
@@ -42,6 +47,11 @@ int main(){
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &numerodepontosturisticos1);
 
+    densidade1 = (float) populacao1 / areaemkm²1;
+    pibpercapta1 =  (float) PIB1 / populacao1;
+
+    super1 = populacao1 + areaemkm²1 + PIB1 + numerodepontosturisticos1 + pibpercapta1 + (1 / densidade1);
+
     //Cadastro da carta 2
     printf("Digite o nome do Estado: \n");
     scanf(" %c", &Estado2);
@@ -64,7 +74,37 @@ int main(){
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &numerodepontosturisticos2);
 
+    densidade2 = (float) populacao2 / areaemkm²2;
+    pibpercapta2 = (float) PIB2 / populacao2;
+
+    super2 = populacao2 + areaemkm²2 + PIB2 + numerodepontosturisticos2 + pibpercapta2 + (1 / densidade2);
+
+    //Exibição dos dados da carta 1
+    printf("\nCarta1\n");
+    printf("O nome do Estado é: %c\n", Estado1);
+    printf("O código da carta 1 é: %s\n", codigodacarta1);
+    printf("O nome da cidade é: %s\n", nomedacidade1);
+    printf("A população é de %d milhões de habitantes\n", populacao1);
+    printf("A cidade tem %.2f km²\n", areaemkm²1);
+    printf("O valor do PIB é de R$ %.2f bilhões\n", PIB1);
+    printf("A cidade tem %d pontos turísticos\n", numerodepontosturisticos1);
+    printf("A densidade populacional é: %.2f hab/km²\n", densidade1);
+    printf("PIB per capta: %.2f reais\n", pibpercapta1);
+
+    //Exibição dos dados da carta 2
+    printf("\nCarta2\n");
+    printf("O nome do Estado é: %c\n", Estado2);
+    printf("O código da carta 2 é: %s\n", codigodacarta2);
+    printf("O nome da cidade é: %s\n", nomedacidade2);
+    printf("A população é de %d milhões de habitantes\n", populacao2);
+    printf("A cidade tem %.2f km²\n", areaemkm²2);
+    printf("O valor do PIB é de R$ %.2f bilhões\n", PIB2);
+    printf("A cidade tem %d pontos turísticos\n", numerodepontosturisticos2);
+    printf("A densidade populacional é: %.2f hab/km²\n", densidade2);
+    printf("PIB per capta: %.2f reais\n", pibpercapta2);
 
 
+
+    return 0;
 }
 
